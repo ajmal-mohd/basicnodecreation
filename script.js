d60befe198d7f2850edb63a97d6711756f224f38
ajmalpk <ajmalpk016@gmail.com>
2024-08-26 11:18:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const path = require("path")
+const fs= require("fs")
+
+
+//  folder creation
+
+// fs.mkdir(path.join(__dirname,"text"),{},(err)=>{
+//     if(err) throw err;
+//     console.log("folder create")
+// })
+
+
+
+// fileCREation and writle file
+
+// fs.writeFile(path.join(__dirname,"text","index.txt"),"content: how to productice",{},(err)=>{
+
+//     if(err) throw err;
+//     console.log("succuss fully  writed");
+// })
+
+
+// multple values writing
+
+
+// fs.appendFile(path.join(__dirname,"/text","index.txt"),"content:how to logic learn \n how to learn displine",(err)=>{
+
+//     if(err) throw err;
+//     console.log("succsuss fully writed");
+// })
+
+
+// reading folder
+
+
+// fs.readFile(path.join(__dirname,"/text","index.txt"), "utf8",(err,data)=>{
+
+//  if(err) throw err;
+//  console.log(data)
+// })
+
+
+// remove file
+// fs.unlink(path.join(__dirname,"/text","index.txt"),{recursive:true},(err)=>{
+//     if(err) throw err;
+//     console.log("succuss fulle deletetd")
+// })
+
+
+// remove folder
+
+// fs.rmdir(path.join(__dirname,"/text"),{recursive:true},(err)=>{
+//     if(err) throw err;
+//     console.log("succuss fulle deletetd")
+// })
+
+
